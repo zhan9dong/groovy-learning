a50ed2d830409104b30c7db56b77a2e2f9d8adab
richardgong1987 <909253305@qq.com>
2018-07-06 17:20:44 +0800

    a

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,6 +1,6 @@
-def list = new ObservableList()
-def printer = { e -> println e.class }
-list.addPropertyChangeListener(printer)
-list.add 'Harry Potter'
-list.add 'Hermione Granger'
-list.remove(0)
+def a(arg = "abc") { println(arg) }
+
+
+a()
+
+a('hhakakakakkakaksksksks')
