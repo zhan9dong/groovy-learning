54c599fd2a96b0d7edd6a8c3a41a0c5740c4a86c
richardgong1987 <909253305@qq.com>
2018-07-06 20:59:55 +0800

    a

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,5 +1,11 @@
-def foo = 1
-def bar = 2
-def map = [(foo): bar]
+def totalPrices(prices, selector) {
+    int total = 0
+    for (int price : prices)
+        if (selector(price)) total += price
+    total
+}
 
-println map
+println totalPrices([1,2,3,4,5,6,7,8,9,10]) { it < 10 }
+println totalPrices([1,2,3,4,5,6,7,8,9,10,11,12]) { it > 10 }
+println totalPrices([1,2,3,4,5]) { true }
+
