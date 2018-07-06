4816908222ef6b01774c1d86bb37b4eb2fb1ee88
richardgong1987 <909253305@qq.com>
2018-07-06 21:17:29 +0800

    a

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,11 +1,1 @@
-def totalPrices(prices, selector) {
-    int total = 0
-    for (int price : prices)
-        if (selector(price)) total += price
-    total
-}
-
-println totalPrices([1,2,3,4,5,6,7,8,9,10]) { it < 10 }
-println totalPrices([1,2,3,4,5,6,7,8,9,10,11,12]) { it > 10 }
-println totalPrices([1,2,3,4,5]) { true }
 
